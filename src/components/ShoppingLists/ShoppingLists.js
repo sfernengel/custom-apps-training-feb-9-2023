ac4eeeef4791dfff82222f4d85c5d8506ab4857d
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2023-02-09 11:33:52 +0100

    step 2

--- src/components/ShoppingLists/ShoppingLists.js
+++ src/components/ShoppingLists/ShoppingLists.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { useQuery } from "@apollo/client/react";
+import { useQuery, useMutation } from "@apollo/client/react";
 import { GRAPHQL_TARGETS } from "@commercetools-frontend/constants";
 import { fetchShoppingLists } from "./queries.graphql";
+import { deleteShoppingList } from "./mutations.graphql";
 
 const ShoppingLists = () => {
   const { error, data, loading } = useQuery(fetchShoppingLists, {
     context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
   });
 
-  if (loading) return "Loading";
+  const options = {
+    refetchQueries: [
+      {
+        query: fetchShoppingLists,
+        context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
+      },
+    ],
+  };
+
+  const [delShoppingList] = useMutation(deleteShoppingList, options);
+
+  const handleDelete = async () => {
+    const { error } = await delShoppingList({
+      variables: {
+        version: 1,
+        id: "e1a95153-f905-4a20-9cdd-89a18c0f5fbb",
+      },
+      context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
+    });
+    if (error) console.log(error.message);
+  };
+
+  if (loading) return "Loading ...";
   if (error) return `--Error ${error.message}`;
   console.log(data);
 
-  return <div>ShoppingLists</div>;
+  return (
+    <div>
+      ShoppingLists
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 };
 ShoppingLists.displayName = "ShoppingLists";
 
