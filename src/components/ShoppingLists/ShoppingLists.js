5b205522dcea36651b73713a3bd4c6679f48839c
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2023-02-09 11:05:48 +0100

    step 1

--- src/components/ShoppingLists/ShoppingLists.js
+++ src/components/ShoppingLists/ShoppingLists.js
@@ -1,13 +1,19 @@
-import React from 'react';
-
-
-
-
+import React from "react";
+import { useQuery } from "@apollo/client/react";
+import { GRAPHQL_TARGETS } from "@commercetools-frontend/constants";
+import { fetchShoppingLists } from "./queries.graphql";
 
 const ShoppingLists = () => {
-   
-  return <div>ShoppingLists</div>
+  const { error, data, loading } = useQuery(fetchShoppingLists, {
+    context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
+  });
+
+  if (loading) return "Loading";
+  if (error) return `--Error ${error.message}`;
+  console.log(data);
+
+  return <div>ShoppingLists</div>;
 };
-ShoppingLists.displayName = 'ShoppingLists';
+ShoppingLists.displayName = "ShoppingLists";
 
 export default ShoppingLists;
