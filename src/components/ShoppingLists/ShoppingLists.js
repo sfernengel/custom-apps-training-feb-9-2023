ed54ee159a5950b5645bc48dbf0efde17d925526
Sevtap Fernengel <92039320+sfernengel@users.noreply.github.com>
2023-02-10 09:01:58 +0100

    Step 3

--- src/components/ShoppingLists/ShoppingLists.js
+++ src/components/ShoppingLists/ShoppingLists.js
@@ -2,13 +2,19 @@ import React from "react";
 import { useQuery, useMutation } from "@apollo/client/react";
 import { GRAPHQL_TARGETS } from "@commercetools-frontend/constants";
 import { fetchShoppingLists } from "./queries.graphql";
-import { deleteShoppingList } from "./mutations.graphql";
+import { deleteShoppingList, createShoppingList } from "./mutations.graphql";
+import { useState } from "react";
 
 const ShoppingLists = () => {
+  const [shoppingListName, setShoppingListName] = useState("");
+  const [shoppingListLocale, setShoppingListLocale] = useState("");
+
+  // fetch shoppinglists
   const { error, data, loading } = useQuery(fetchShoppingLists, {
     context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
   });
 
+  // delete a shopping list
   const options = {
     refetchQueries: [
       {
@@ -24,10 +30,52 @@ const ShoppingLists = () => {
     const { error } = await delShoppingList({
       variables: {
         version: 1,
-        id: "e1a95153-f905-4a20-9cdd-89a18c0f5fbb",
+        id: "77610ac9-e2b4-49d8-819d-47107a2a47ab",
       },
       context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
     });
+    if (error) console.log(error.message);
+  };
+
+  // create a shopping list
+  // locale:en name:"abcd"
+
+  const [addShoppingList] = useMutation(createShoppingList, options);
+
+  // const handleAdd = async () => {
+  //   const { error } = await addShoppingList({
+  //     variables: {
+  //       name: "My Shopping List 123",
+  //       locale: "en",
+  //     },
+  //     context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
+  //   });
+
+  //   if (error) console.log(error.message);
+  // };
+
+  const handleAdd = async () => {
+    const { error } = await addShoppingList({
+      variables: {
+        name: "My Shopping List 123",
+        locale: "en",
+      },
+      context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
+    });
+
+    if (error) console.log(error.message);
+  };
+
+  const handleAddShoppingList = async (event) => {
+    event.preventDefault();
+    const { error } = await addShoppingList({
+      variables: {
+        name: shoppingListName,
+        locale: shoppingListLocale,
+      },
+      context: { target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM },
+    });
+
     if (error) console.log(error.message);
   };
 
@@ -38,7 +86,23 @@ const ShoppingLists = () => {
   return (
     <div>
       ShoppingLists
+      <form>
+        <label>Locale: </label>
+        <input
+          type="text"
+          value={shoppingListLocale}
+          onChange={(event) => setShoppingListLocale(event.target.value)}
+        />
+        <label>Name: </label>
+        <input
+          type="text"
+          value={shoppingListName}
+          onChange={(event) => setShoppingListName(event.target.value)}
+        />
+        <button onClick={handleAddShoppingList}> add Shopping List</button>
+      </form>
       <button onClick={handleDelete}>Delete</button>
+      <button onClick={handleAdd}>Add</button>
     </div>
   );
 };
